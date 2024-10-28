afb1959ab7a429ad8d4c0264b342d110b566b53e
chou27 <soumith273@gmail.com>
2024-10-28 20:51:13 +0530

    Initial commit

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-black p-0 m-0">
+        <Image 
+          src="/streamhive.svg"
+          alt="StreamHive"
+          height={120} 
+          width={120} 
+          className="border-none" 
+        />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">StreamHive</p>
+        <p className="text-sm text-muted-foreground">Let's Stream</p>
+      </div>
+    </div>
+  );
+};
---
+++ lib/db.ts
@@ -0,0 +1,18 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global{
+//     var prisma: PrismaClient | undefined;
+// }
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if(process.env.NODE_ENV !== "production") globalThis.prisma = db
+
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const db = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') global.prisma = db
