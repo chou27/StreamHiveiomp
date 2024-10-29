18db49f2c84e9483d7c8ea07264e597ac1a14d0a
chou27 <soumith273@gmail.com>
2024-10-30 00:13:30 +0530

    Update code with daily changes

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -5,6 +5,7 @@ import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 export const Toggle = () => {
@@ -14,7 +15,7 @@ export const Toggle = () => {
         onCollapse
     } = useSidebar((state) => state);
 
-    const label = collapsed? "Expand" : "Collapse";
+    const label = collapsed ? "Expand" : "Collapse";
     return(
         <>
             {collapsed && (
@@ -47,5 +48,14 @@ export const Toggle = () => {
                 </div>
             )}
         </>
+    );
+};
+
+export const ToggleSkeleton = () => {
+    return(
+        <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+            <Skeleton className="h-6 w-[100px]"/>
+            <Skeleton className="h-6 w-6"/>
+        </div>
     )
 }
