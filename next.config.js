4115457ccba7ea7e16f5072ae22808cf869df046
chou27 <soumith273@gmail.com>
2024-11-07 04:13:19 +0530

    final deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // other config options
-}
+  experimental: {
+    serverActions: true,
+  },
+};
 
 module.exports = nextConfig;
