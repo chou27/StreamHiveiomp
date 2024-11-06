7112b7c3cf0f7c77d249ff890810cadc4e403488
chou27 <soumith273@gmail.com>
2024-11-07 03:16:29 +0530

    Final Deployment

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,88 +1,22 @@
-// import { StreamPlayer } from "@/components/stream-player";
-// import { getUserByUsername } from "@/lib/user-service";
-// import { currentUser } from "@clerk/nextjs/server";
-
-// interface CreatorPageProps {
-//     params: {
-//         username: string;
-//     };
-// }
-
-// const CreatorPage = async ({ params }: CreatorPageProps) => {
-//     const externalUser = await currentUser();
-//     const user = await getUserByUsername(params.username);
-
-//     if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
-//         throw new Error("Unauthorized");
-//     }
-
-//     return (
-//         <div className="h-full">
-//             <StreamPlayer 
-//                 user={user}
-//                 stream={user.stream}
-//                 isFollowing
-//             />
-//         </div>
-//     );
-// }
-
-// export default CreatorPage;
-
-
-
-// import { StreamPlayer } from "@/components/stream-player";
-// import { getUserByUsername } from "@/lib/user-service";
-// import { currentUser } from "@clerk/nextjs/server";
-
-// interface CreatorPageProps {
-//     params: {
-//         username: string;
-//     };
-// };
-
-
-// const CreatorPage = async ({
-//     params,
-// } : CreatorPageProps) => {
-//     const externalUser = await currentUser();
-//     const user = await getUserByUsername(params.username);
-
-//     if(!user || user.externalUserId !== externalUser?.id || !user.stream) {
-//         throw new Error("Unauthorized");
-//     }
-
-//     return( 
-//         <div className="h-full">
-//             <StreamPlayer 
-//              user = {user}
-//              stream = {user.stream}
-//              isFollowing
-//             />
-//         </div>
-//     );
-// }
-
-// export default CreatorPage;
-
 import { StreamPlayer } from "@/components/stream-player";
 import { getUserByUsername } from "@/lib/user-service";
 import { currentUser } from "@clerk/nextjs/server";
 
-interface CreatorPageProps {
+type CreatorPageProps = {
     params: {
         username: string;
     };
 };
 
-const CreatorPage = async ({
-    params,
-}: CreatorPageProps) => {
+const CreatorPage = async ({ params }: CreatorPageProps) => {
+    // Destructure username from params
     const { username } = params;
 
+    // Get the current user and the user by username
     const externalUser = await currentUser();
     const user = await getUserByUsername(username);
 
+    // Check for authorization
     if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
         throw new Error("Unauthorized");
     }
