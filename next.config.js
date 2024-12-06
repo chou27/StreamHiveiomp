af8b3e6d9f3c5b38e99c22b7b44adc376137a17c
chou27 <soumith273@gmail.com>
2024-12-06 17:18:16 +0530

    finaldeployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: true,
+    serverComponentsExternalPackages: ["@clerk/clerk-sdk-node"],
+  },
+  images: {
+    domains: [
+      "utfs.io",  // for uploadthing
+      "img.clerk.com"  // for clerk avatars
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+      url: require.resolve('url'),
+    };
+    return config;
   },
 };
 
