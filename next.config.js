ea2c05ab5b83726c818ce7b0cbc6f12ca7fb5225
chou27 <soumith273@gmail.com>
2024-12-06 18:05:44 +0530

    finaldeployment4

--- next.config.js
+++ next.config.js
@@ -28,6 +28,37 @@
 
 // module.exports = nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: [
+//       "utfs.io",
+//       "img.clerk.com"
+//     ]
+//   },
+//   experimental: {
+//     serverComponentsExternalPackages: ["@clerk/clerk-sdk-node"],
+//   },
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback = {
+//         ...config.resolve.fallback,
+//         fs: false,
+//         net: false,
+//         tls: false,
+//         crypto: require.resolve('crypto-browserify'),
+//         stream: require.resolve('stream-browserify'),
+//         path: require.resolve('path-browserify'),
+//         url: require.resolve('url'),
+//       };
+//     }
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -39,6 +70,7 @@ const nextConfig = {
   },
   experimental: {
     serverComponentsExternalPackages: ["@clerk/clerk-sdk-node"],
+    instrumentationHook: true  // Added this line
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
